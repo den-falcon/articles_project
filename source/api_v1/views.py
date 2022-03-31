4a2f4b87a86fe88876e24a86b33e56c831a73875
denis-sokolov <den.s.glad@gmail.com>
2022-03-31 22:02:33 +0300

    Создано приложение api_v1. Добавлен модельный сериализатор, созданы методы get, put, delete в api_v1.views

---
+++ source/api_v1/views.py
@@ -0,0 +1,48 @@
+from http import HTTPStatus
+
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from api_v1.serializers import ArticleSerializer
+from webapp.models import Article
+
+
+class ArticleListView(APIView):
+    serializer_class = ArticleSerializer
+
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()
+        serializer = self.serializer_class(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        article = serializer.save(author=request.user)
+        return Response(
+            self.serializer_class(article).data,
+            status=HTTPStatus.CREATED
+        )
+
+
+class ArticleSingleObjectView(APIView):
+    serializer_class = ArticleSerializer
+
+    def get(self, request, *args, pk=None, **kwargs):
+        article = get_object_or_404(Article, pk=pk)
+        serializer = self.serializer_class(article)
+        return Response(serializer.data)
+
+    def delete(self, request, *args, pk=None, **kwargs):
+        article = get_object_or_404(Article, pk=pk)
+        serializer = self.serializer_class(article)
+        article.delete()
+        return Response(serializer.data, status=HTTPStatus.NO_CONTENT)
+
+    def put(self, request, *args, pk=None, **kwargs):
+        article = get_object_or_404(Article, pk=pk)
+        serializer = self.serializer_class(data=request.data, instance=article)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data)
--- source/hello/settings.py
+++ source/hello/settings.py
@@ -35,10 +35,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 
     # Custom apps
     'webapp',
-    'accounts'
+    'accounts',
+    'api_v1',
 
     # Lib apps
 ]
